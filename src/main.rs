40dacfa3cc0bbd51fca1511eee16e3f733503745
Aleksandr Kostikov <alexko@microsoft.com>
2022-10-10 12:44:45 -0700

    separated cli into a module

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,11 @@
 /// Clap documentation: https://docs.rs/clap/latest/clap/
 /// Environment variables: https://web.mit.edu/rust-lang_v1.25/arch/amd64_ubuntu1404/share/doc/rust/html/book/second-edition/ch12-05-working-with-environment-variables.html
+mod cli;
 
 use std::path::{Path, PathBuf};
-use clap::{Parser, Subcommand};
+use clap::Parser;
 use walkdir::WalkDir;
-
-/// Fast folder walker to be used as replacement for the default fzf walker
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Cli {
-    /// Add files to the output (cdf / codef)
-    #[arg(short = 'f', long, value_name = "true|false")]
-    add_files: bool,
-
-    /// Add the current folder to the output
-    #[arg(short = 'c', long)]
-    add_current_folder: bool,
-
-    /// Add entries that start with dot to the output (hidden on unix systems)
-    #[arg(short = '.', long, value_name = "dots")]
-    add_dots: bool,
-
-    /// Add entries with hidden NTFS attribute to the output (hidden on windows systems)
-    #[arg(short = 'n', long, value_name = "hidden")]
-    add_hidden: bool,
-
-    /// List the most deep entries first
-    #[arg(short = 'l', long, value_name = "leaf")]
-    leafs_first: bool,
-
-    /// Traverse symbolic links
-    #[arg(short = 't', long, value_name = "link")]
-    link_traversal: bool,
-
-    /// Maximum depth of traversal, unlimited by default
-    #[arg(short, long)]
-    depth: usize,
-
-    /// List of injected entries (favorites)
-    #[arg(short, long)]
-    injected: Vec<PathBuf>,
-
-    /// List of excluded entry names
-    #[arg(short, long)]
-    excluded: Vec<PathBuf>,
-}
+use cli::Cli;
 
 /// erer
 fn main() {
