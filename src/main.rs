adf08e62407d958bf5b9a7a99c6e4367aa9b4ad1
Aleksandr Kostikov <alexko@microsoft.com>
2022-10-10 12:05:53 -0700

    fixing build error

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,25 @@ https://github.com/clap-rs/clap/tree/45241277043f2a8cc64230e18574b88b005e765c/ex
 https://web.mit.edu/rust-lang_v1.25/arch/amd64_ubuntu1404/share/doc/rust/html/book/second-edition/ch12-05-working-with-environment-variables.html
 */
 
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use clap::{Parser, Subcommand};
 use walkdir::WalkDir;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
-struct Cli<'a> {
+struct Cli {
     /// Optional list of excluded folders names (taken from file)
-    excluded: Option<&'a [Path]>,
+    excluded: Option<Vec<PathBuf>>,
 
     /// Flag that says if files are needed in the output (cdf / codef)
-    addFiles: bool,
+    add_files: bool,
 
     /// Flag that says if the current folder is needed in the output
-    addCurrentFolder: bool,
+    add_current_folder: bool,
 
     /// Optional list of injected folders (favorites)
-    injected: Option<&'a [Path]>,
+    injected: Option<Vec<PathBuf>>,
 
     /// Optional maximum depth of traversal, unlimited by default
     #[arg(short, long)]
@@ -30,19 +30,19 @@ struct Cli<'a> {
 
     /// Flag that says if the most deep entries need to be displayed first
     #[arg(short, long, value_name = "leaf")]
-    leafsFirst: bool,
+    leafs_first: bool,
 
     /// Flag that says if the symbolic links need to be traversed
     #[arg(short, long, value_name = "link")]
-    linkTraversal: bool,
+    link_traversal: bool,
 
     /// Flag that says if the entries that start with dot need to be skipped (hidden on unix systems)
     #[arg(short, long, value_name = ".")]
-    skipDots: bool,
+    skip_dots: bool,
 
     /// Flag that says if the entries that have hidden NTFS attribute need to be skipped (hidden on windows systems)
     #[arg(short, long, value_name = "h")]
-    skipHidden: bool,
+    skip_hidden: bool,
 }
 
 fn main() {
